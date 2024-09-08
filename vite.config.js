7e1aad7052d823badb2efe40b2a4ad63b68d103f
Administrator <raulgm00@gmail.com>
2024-09-07 19:35:02 -0600

    Instalacion de dependencias

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], server: {
+    hmr: {
+      overlay: true,  // Esto garantiza que los errores se sigan mostrando en el overlay
+    },
+  },
 })
